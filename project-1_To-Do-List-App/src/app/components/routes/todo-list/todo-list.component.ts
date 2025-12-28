3312a2c766815dde7653e200f65722a27f0fa76e
Arash44 <arashasghari1245@gmail.com>
2025-12-28 13:18:48 +0330

    acoordion responsiveness improved

--- project-1_To-Do-List-App/src/app/components/routes/todo-list/todo-list.component.ts
+++ project-1_To-Do-List-App/src/app/components/routes/todo-list/todo-list.component.ts
@@ -1,11 +1,13 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { db, Todo } from '../../../services/db/todo-db.service';
 import { liveQuery } from 'dexie';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { DatePipe } from '@angular/common';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-todo-list',
-  imports: [MatExpansionModule],
+  imports: [MatExpansionModule, DatePipe],
   templateUrl: './todo-list.component.html',
   standalone: true,
   host:{
@@ -14,10 +16,21 @@ import { MatExpansionModule } from '@angular/material/expansion';
 })
 export class TodoListComponent {
   todos = signal<Todo[]>([]);
+  isSmallScreen = signal(false);
+
+  private breakpointObserver = inject(BreakpointObserver);
 
   constructor() { 
     liveQuery(() => db.todos.toArray()).subscribe(all => {
       this.todos.set(all);
     }); 
+  }
+
+  ngOnInit() { 
+    this.breakpointObserver
+      .observe([Breakpoints.Small, Breakpoints.XSmall]) 
+      .subscribe(result => { 
+        this.isSmallScreen.set(result.matches); 
+      }); 
   }
 }
