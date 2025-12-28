58f566bc5c850a9b22ffd2e4734d0da1e8b35b99
Arash44 <arashasghari1245@gmail.com>
2025-12-28 12:14:14 +0330

    add accordions for todos

--- project-1_To-Do-List-App/src/app/components/routes/todo-list/todo-list.component.ts
+++ project-1_To-Do-List-App/src/app/components/routes/todo-list/todo-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { db, Todo } from '../../../services/db/todo-db.service';
 import { liveQuery } from 'dexie';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-todo-list',
-  imports: [],
+  imports: [MatExpansionModule],
   templateUrl: './todo-list.component.html',
   standalone: true,
+  host:{
+    class: "w-[clamp(250px,80%,700px)]"
+  }
 })
 export class TodoListComponent {
   todos = signal<Todo[]>([]);
